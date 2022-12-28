37427328db38dbfdc6a4fe5a08e3791eff3f5483
unknown <tainm.1999@gmail.com>
2022-12-28 16:29:10 +0700

    update

--- Frontend/src/redux/store.js
+++ Frontend/src/redux/store.js
@@ -1,16 +1,7 @@
 import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import cartReducer from "./cartRedux";
 import userReducer from "./userRedux";
-import {
-  persistStore,
-  persistReducer,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from "redux-persist";
+import {persistStore,persistReducer,FLUSH,REHYDRATE,PAUSE,PERSIST,PURGE,REGISTER,} from "redux-persist";
 import storage from "redux-persist/lib/storage";
 
 const persistConfig = {
